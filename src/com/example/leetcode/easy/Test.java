d1b999fa3e399441d28dd3aabd5232026e7bf600
Tyler Kummer <tylerkummer96@gmail.com>
2021-02-03 16:31:13 -0600

    two sum improvement

--- src/com/example/leetcode/easy/Test.java
+++ src/com/example/leetcode/easy/Test.java
@@ -2,17 +2,5 @@ package com.example.leetcode.easy;
 
 public class Test
 {
-    public static void main(String[] args)
-    {
-        int num = 12345;
-        int reverse = 0;
 
-        while (num != 0)
-        {
-            int remainder = num % 10;
-            reverse = reverse * 10 + remainder;
-            num = num /10;
-        }
-        System.out.println(reverse);
-    }
 }
--- src/com/example/leetcode/easy/TwoSum.java
+++ src/com/example/leetcode/easy/TwoSum.java
@@ -2,28 +2,22 @@ package com.example.leetcode.easy;
 
 public class TwoSum
 {
-    // Create a method that will return an array of integers
-    // Take in an array of integers called nums and an integer call target as arguments
-    public int[] twoSum(int[] nums, int target)
-    {
-        // Create a for loop that will loop through the array nums
-        for (int i = 0; i < nums.length; i++)
-        {
-            // Create another for loop that will loop through the array after nums[i]
-            for (int j = i + 1; j < nums.length; j++)
-            {
-                // Check if the value of nums[j] will result in the target number minus the value of nums[i]
-                if (nums[j] == target - nums[i])
-                {
-                    // Return a new array of integers that is composed of the values i and j.
-                    return new int[] { i, j };
-                }
+    public int[] twoSum(int[] nums, int target) {
+        // Create an index of 0
+        int i = 0;
+        // Create a for loop that goes through the next element
+        for(int j = i + 1; j < nums.length; j++){
+            // Check if the element of j is target subtracted from index i
+            if(nums[j] == target - nums[i]){
+                // If it is then return a new array containing i and j
+                return new int[] {i, j};
+            } else{
+                // If not then cycle to the next element of i
+                i += 1;
             }
         }
-
-        // If we exit out of the for loop then none of the sums matched our target.
-        // Return a message that displays that no matches were found.
+        // Return this message if nothing passes, will allow user to return a string instead of an array of nums
         throw new IllegalArgumentException("No Match");
-
     }
 }
+
